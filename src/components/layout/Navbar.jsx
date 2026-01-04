6df0883ce9c18064a6f6ee2b885b1b1103da86ed
Suyog Gautam <sakurachhetri97@gmail.com>
2026-01-04 19:18:05 +0545

    added courses for bbmbca

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = ({ currentPage, setCurrentPage, isSidebarOpen, setIsSidebarOpen }
   const unreadCount = notifications.filter(n => !n.read).length;
 
   return (
-    <nav className="bg-white shadow-lg fixed w-full top-0 z-50 border-b border-gray-100">
+    <nav className="bg-purple-300 shadow-lg fixed w-full top-0 z-50 border-b border-gray-100 ">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="flex items-center space-x-3">
